914d01f627ebc84021758ac1433e55f312523e90
dracospitfire <austin3flores@gmail.com>
2022-04-20 19:59:34 +0000

    added geometry module

---
+++ myMath/geometry.py
@@ -0,0 +1,14 @@
+#Constant PI
+PI = 3.141592653589793238462643383279
+
+def area_of_square(side):
+    #This function determins the area of a square.
+    return side ** 2
+
+def area_of_rectangle(side1, side2):
+    #This function determins the area of a rectangle.
+    return side1 * side2
+
+def area_of_cicle(radius):
+    #This function determins the area of a circle.
+    return (radius ** 2)*PI
